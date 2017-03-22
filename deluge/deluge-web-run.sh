684596e9c2ebb47d101509c9b504b64c5201f805
joe miller <joeym@joeym.net>
2017-03-22 07:54:51 -0700

    add with-contenv to s6-overlay run scripts so that container ENV vars are propagated

--- deluge/deluge-web-run.sh
+++ deluge/deluge-web-run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/execlineb -P
+with-contenv
 s6-setuidgid nobody
 /usr/bin/with-contenv deluge-web -c /config -p 8083
--- deluge/deluged-run.sh
+++ deluge/deluged-run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/execlineb -P
+with-contenv
 s6-setuidgid nobody
 /usr/bin/with-contenv deluged -d -c /config -p 9001 -L info
