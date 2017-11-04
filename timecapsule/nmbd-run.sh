0a2323b7c1c34ffafd272c1f4c0f680bf67e74b9
joe miller <joeym@joeym.net>
2017-11-04 10:15:47 -0700

    updated to latest samba master branch

--- timecapsule/nmbd-run.sh
+++ timecapsule/nmbd-run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/execlineb -P
-/usr/bin/with-contenv /usr/local/samba/sbin/nmbd --foreground --log-stdout
+#/usr/bin/with-contenv /usr/local/samba/sbin/nmbd -i --log-stdout
+/usr/bin/with-contenv /usr/local/samba/sbin/nmbd -D -d 3 </dev/null
+#/usr/bin/with-contenv /usr/local/samba/sbin/nmbd --foreground -i --log-stdout -d 3 </dev/null
--- timecapsule/smbd-run.sh
+++ timecapsule/smbd-run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/execlineb -P
 # add '-d <debuglevel>' (1, 2, 3 are good) for debug logging
-/usr/bin/with-contenv /usr/local/samba/sbin/smbd --foreground --log-stdout
+#/usr/bin/with-contenv /usr/local/samba/sbin/smbd -i --log-stdout
+#/usr/bin/with-contenv /usr/local/samba/sbin/smbd --log-stdout -d 3 </dev/null
+/usr/bin/with-contenv /usr/local/samba/sbin/smbd -D </dev/null
+#/usr/bin/with-contenv /usr/local/samba/sbin/smbd --foreground -i --log-stdout -d 3 </dev/null
