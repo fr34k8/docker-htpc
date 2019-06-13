2982be8e342f758be8c650b0bc482cc0719fa4bd
joe miller <joeym@joeym.net>
2019-06-13 07:01:13 -0700

    deluge: bump to v2.0.0

--- deluge/deluge-web-run.sh
+++ deluge/deluge-web-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/execlineb -P
 with-contenv
 s6-setuidgid nobody
-/usr/bin/with-contenv deluge-web -c /config -p 8083
+/usr/bin/with-contenv deluge-web -c /config --do-not-daemonize -p 8083
